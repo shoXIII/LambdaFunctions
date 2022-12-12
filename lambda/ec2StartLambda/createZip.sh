d845e570bc19367da691f3286dc42eab9d32f814
shoXIII <xlll.vermelho@gmail.com>
2022-12-13 00:17:27 +0900

    chore:ディレクトリ名を修正

--- lambda/ec2StartLambda/createZip.sh
+++ lambda/ec2StartLambda/createZip.sh
@@ -3,18 +3,18 @@ set -e
 
 ROOT=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ../..; pwd);
 
-cd "$ROOT/lambda/ec2-start-up-lambda"
+cd "$ROOT/lambda/ec2StartLambda"
 
 npm ci
 npm run tsc
 
 cp package*.json ./dist
-cd "$ROOT/lambda/ec2-start-up-lambda/dist"
+cd "$ROOT/lambda/ec2StartLambda/dist"
 
 npm ci --omit=dev
 
 zip -r ../index .
 
-cd "$ROOT/lambda/ec2-start-up-lambda"
+cd "$ROOT/lambda/ec2StartLambda"
 
 rm -rf ./dist
