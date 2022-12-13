9d771e8087a2e8605336c4b4aa1d36aae45631b9
shoXIII <xlll.vermelho@gmail.com>
2022-12-13 21:38:13 +0900

    feat:インスタンス停止用のラムダを作成

---
+++ lambda/ec2StopLambda/src/index.ts
@@ -0,0 +1,38 @@
+import { EC2 } from "@aws-sdk/client-ec2";
+
+const INSTANCE_ID = process.env['INSTANCE_ID'];
+if (INSTANCE_ID === undefined) {
+    throw new Error('INSTANCE_ID not found')
+}
+const INSTANCE_REGION = process.env['INSTANCE_REGION'];
+if (INSTANCE_REGION === undefined) {
+    throw new Error('INSTANCE_REGION not found')
+}
+
+const stopInstance = (ec2: EC2) => {
+    const stopInstanceParam = {
+        InstanceIds: [
+            INSTANCE_ID
+        ]
+    }
+    return new Promise((resolve, reject) => {
+        ec2.stopInstances(stopInstanceParam, (err: any, data: any) => {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+                return
+            } 
+            console.log(data);
+            resolve;
+        });
+    });
+}
+
+exports.handler = async () => {
+    console.log(`try to stop instance \n INSTANCE_ID: ${INSTANCE_ID}`);
+    const ec2 = new EC2({
+        region: INSTANCE_REGION,
+        apiVersion: 'latest'
+    });
+    await stopInstance(ec2);
+};
